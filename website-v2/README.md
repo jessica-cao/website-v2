b40fab0fa261184ebee423f13c94608951b7553e
jessica-cao <jessicacao1756@gmail.com>
2020-01-04 20:38:11 -0500

    add background and landing text

---
+++ website-v2/README.md
@@ -0,0 +1,5 @@
+# website-v2
+personal website 2: electric boogaloo
+
+Version 2 of my personal website. Version 1 can be found [right here](https://github.com/jessica-cao/jessica-cao.github.io)
+Built with react.js
---
+++ website-v2/website-v2/src/scripts/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h5>
+        Computer Science student | Web development enthusiast | Crippling Optimist
+        </h5>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
